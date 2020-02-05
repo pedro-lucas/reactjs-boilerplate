1ffeff3937f293a94011b3bdfbd0cd1ab4c6e1f5
Pedro Lucas <pedro@objota.com.br>
2020-02-05 13:37:32 -0300

    Adicionado exemplo de teste

--- __tests__/logIn.test.js
+++ __tests__/logIn.test.js
@@ -1,14 +1,25 @@
 import { cloneableGenerator } from '@redux-saga/testing-utils';
 import { signIn } from '~/store/modules/auth/sagas';
-import { SIGN_IN_SUCCESS } from '~/store/modules/auth/actions';
-import env from '~/config/Environment';
+import { SIGN_IN_SUCCESS, SIGN_FAILURE } from '~/store/modules/auth/actions';
 
 test('Deve permitir o login do usuário', () => {
    const gen = cloneableGenerator(signIn)({
-      payload: { username: env.API_TEST_USER, password: env.API_TEST_PASSWORD },
+      payload: { username: 'username', password: 'password' },
    });
    gen.next();
-   expect(gen.next().value.payload.action).toEqual({
-      type: SIGN_IN_SUCCESS,
+   expect(SIGN_IN_SUCCESS).toEqual(
+      gen.next({
+         data: { token: 'xxxx', user: { id: 1, name: 'teste' } },
+      }).value.payload.action.type
+   );
+});
+
+test('Deve gerar uma exceção no login do usuário', () => {
+   const gen = cloneableGenerator(signIn)({
+      payload: { username: 'username', password: 'password' },
    });
+   gen.next();
+   expect(SIGN_FAILURE).toEqual(
+      gen.throw('Falha na autenticação.').value.payload.action.type
+   );
 });
