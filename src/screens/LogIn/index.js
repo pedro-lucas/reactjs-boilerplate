641981fcd23e37e37292da45209bba61171b14c3
Pedro Lucas <pedro@objota.com.br>
2020-02-05 15:17:28 -0300

    Removido unform

--- src/screens/LogIn/index.js
+++ src/screens/LogIn/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+import { Formik, Field, Form } from 'formik';
 
 import { signInRequest } from '~/store/modules/auth/actions';
 
@@ -14,21 +14,28 @@ export default function SignIn() {
    const dispatch = useDispatch();
    const loading = useSelector(state => state.auth.loading);
 
-   function handleSubmit({ username, password }) {
-      dispatch(signInRequest(username, password));
-   }
-
    return (
-      <>
-         <Form schema={schema} onSubmit={handleSubmit}>
-            <strong>SEU USUÁRIO</strong>
-            <Input name="username" placeholder="exemplo@email.com" />
-            <strong>SUA SENHA</strong>
-            <Input name="password" type="password" placeholder="*********" />
-            <button type="submit">
-               {loading ? 'Carregando...' : 'Entrar no Sistema'}
-            </button>
-         </Form>
-      </>
+      <Formik
+         initialValues={{
+            username: '',
+            password: '',
+         }}
+         validationSchema={schema}
+         onSubmit={values => {
+            dispatch(signInRequest(values.username, values.password));
+         }}
+      >
+         {() => (
+            <Form>
+               <strong>SEU USUÁRIO</strong>
+               <Field name="username" placeholder="exemplo@email.com" />
+               <strong>SUA SENHA</strong>
+               <Field name="password" type="password" placeholder="*********" />
+               <button type="submit">
+                  {loading ? 'Carregando...' : 'Entrar no Sistema'}
+               </button>
+            </Form>
+         )}
+      </Formik>
    );
 }
